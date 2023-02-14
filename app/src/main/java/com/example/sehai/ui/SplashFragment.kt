574d3a7bda40b8a5527a0db09fd377bbb3fca5b7
Roisull <lusiro2001@students.amikom.ac.id>
2023-02-15 00:13:59 +0700

    Add LottiesFile in SplashScreen

--- app/src/main/java/com/example/sehai/ui/SplashFragment.kt
+++ app/src/main/java/com/example/sehai/ui/SplashFragment.kt
@@ -1,34 +1,41 @@
 package com.example.sehai.ui
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.navigation.Navigation
 import com.example.sehai.R
-import kotlinx.android.synthetic.*
-import kotlinx.android.synthetic.main.fragment_splash.*
+import com.example.sehai.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_splash, container, false)
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
 
-        return view
+        return binding?.root
     }
-    private fun moveToSignUpFragment(){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListener()
+    }
+
+    private fun initListener(){
+        binding?.run {
+            ivLogoSehaiSplashScreen?.setOnClickListener {
+                gotoSignUp()
+            }
+        }
+    }
+    private fun gotoSignUp(){
         Navigation.findNavController(requireView()).navigate(R.id.action_splashFragment_to_signUpFragment)
-    }
-    private fun moveToSignInFragment(){
-        Navigation.findNavController(requireView()).navigate(R.id.action_splashFragment_to_signInFragment)
     }
 }
